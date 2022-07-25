4dd16929d6e4ae70cee8e95197bbbded7896baa5
Yerin Kim <yerin0827@gmail.com>
2022-07-26 08:51:58 +0900

    템플릿 추가

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -1,12 +1,15 @@
 package com.example.demo.Controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class Controller {
     @GetMapping("/test")
     public String test(){
         return "Hello World!";
     }
+
 }
