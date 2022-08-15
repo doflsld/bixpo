cdcfecc18595218b3718774ed76686a64adcd5a0
Yerin Kim <yerin0827@gmail.com>
2022-08-15 14:52:46 +0900

    bsb 화면 테스트

---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,9 @@
+function initMap(){
+    var kintex = { lat: 37.67086821347, lng: 126.75195117121};
+    var map = new google.maps.Map(
+        document.getElementById('map'),{
+            zoom: 12,
+
+        }
+    )
+}
