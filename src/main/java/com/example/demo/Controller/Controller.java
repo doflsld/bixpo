0a58ed748a669eda16c944ad38656a5b6837a120
Yerin Kim <yerin0827@gmail.com>
2022-08-22 09:21:06 +0900

    bsb 통합관제시스템 메인페이지 수정중

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -2,14 +2,20 @@ package com.example.demo.Controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@org.springframework.stereotype.Controller
 @AllArgsConstructor
 public class Controller {
     @GetMapping("/test")
     public String test(){
-        return "Hello World!";
+        return "test";
+    }
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "index2";
     }
 
 }
