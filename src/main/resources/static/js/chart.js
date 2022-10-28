093841bf1e477e330ee4191cdfbc0c267081e010
doflsld <yerin0827@gmail.com>
2022-10-28 11:38:20 +0900

    Revert "스크립트/코드 정리"
    This reverts commit 1c30e10a64566d8ac216dc1f054937baef35924d.
    
    # Conflicts:
    #	src/main/java/com/example/demo/Controller/Controller.java
    #	src/main/resources/static/js/map.js
    #	src/main/resources/static/js/map_info.js
    #	src/main/resources/static/test/index_info_test.html
    #	src/main/resources/static/test/index_test.html
    #	src/main/resources/templates/index_info.html
    #	src/main/resources/test/index_b_test.html
    #	src/main/resources/test/index_test.html
    #	src/main/resources/test/monitor_test.html

--- src/main/resources/static/js/chart.js
+++ src/main/resources/static/js/chart.js
@@ -1,51 +1,138 @@
+bar_data();
 
-    Highcharts.chart('chart_div', {
-    colors : ['#EF7B00'],
+var arr_min;
+var arr_max;
+var arr_avg;
+
+function bar_data() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://devba.kr/bsb/getData.php',
+        async: false,
+        data: {
+            'modem': '0',
+            'bar': '1',
+        },
+        success: function (data) {
+            arr_avg=[parseFloat(data[0].SRCb00001_T.dv),parseFloat(data[1].SRCb00002_T.dv),parseFloat(data[2].SRCb00003_T.dv),parseFloat(data[3].SRCb00004_T.dv)];
+            arr_min=[parseFloat(data[0].SRCb00001_T.di),parseFloat(data[1].SRCb00002_T.di),parseFloat(data[2].SRCb00003_T.di),parseFloat(data[3].SRCb00004_T.di)];
+            arr_max=[parseFloat(data[0].SRCb00001_T.da),parseFloat(data[1].SRCb00002_T.da),parseFloat(data[2].SRCb00003_T.da),parseFloat(data[3].SRCb00004_T.da)];
+
+
+        }
+    });
+
+}
+
+
+Highcharts.chart('bar-chart', {
     chart: {
-        type: 'area'
+        type: 'column'
     },
     title: {
-        text: ' '
+        text: 'Temperature in Battery Safety Box'
     },
     xAxis: {
-        categories: ['09.13', '09.14', '09.15', '09.16', '09.17', '09.18', '09.19', '09.20', '09.21', '09.22']
+        categories: [
+            'Left',
+            'Right',
+            'Front',
+            'Back',
+        ],
+        crosshair: true
     },
     yAxis: {
-        max: 100,
+        min: 0,
         title: {
-            text: ' '
+            text: 'Temperature (℃)'
         }
     },
-    legend: {
-
-    },
-    // 하단 highchart.com 글씨 삭제
-    credits: {
-        enabled: false
-    },
-    // 상단 context menu 제거
-    exporting: {
-        enabled: false
-    },
     tooltip: {
-        valueSuffix:'%'
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} ℃</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
     },
     plotOptions: {
-        series: {
-
-        },
-        area: {
-            stacking: 'normal',
-            lineColor: '#FFFFFF',
-            lineWidth: 1,
-            marker: {
-                lineWidth: 1,
-                lineColor: '#FFFFFF'
-            }
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
         }
     },
     series: [{
-        name: 'Percent',
-        data: [85, 91, 95, 88, 90, 87, 97, 90, 87, 98]
+        name: 'High',
+        data: arr_max
+
+    }, {
+        name: 'Low',
+        data: arr_min
+
+    }, {
+        name: 'Avg',
+        data: arr_avg
+
     }]
 });
+
+
+var gaugeOptions = {
+    chart: {
+        type: 'solidgauge'
+    },
+
+    title: null,
+
+    pane: {
+        center: ['50%', '85%'],
+        size: '140%',
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+            backgroundColor:
+                Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0.1, '#55BF3B'], // green
+            [0.5, '#DDDF0D'], // yellow
+            [0.9, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        tickWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+            y: -70
+        },
+        labels: {
+            y: 16
+        }
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+            }
+        }
+    }
+};
+
